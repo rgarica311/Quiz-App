3ef06af9327090876441cb541629c7d336b07199
Rory Garcia <rory.garcia1@gmail.com>
2019-05-02 12:13:18 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,202 @@
+function testButton() {
+  console.log('js running')
+  $('.quiz-button').on('click', function(event) {
+    event.preventDefault();
+    $('.bg-image').css({opacity: 1.0, visibility: "visible"}).animate({opacity: 0}, 1000);
+  });
+}
+
+function hideBeginQuiz() {
+  $('.content').on('click', '.quiz-button', function(event){
+    $(event.currentTarget).parent().css('display', 'none');
+    showQuiz()
+  })
+}
+
+let questionNum = 0;
+let currentScore = 0;
+
+function buildQuestion() {
+  if (questionNum < STORE.length) {
+    console.log(`questionNum is ${questionNum}`)
+    return $('.form').html(`<img class="film-image" src="Assets/film-strip2.png">
+    <div class="quiz">
+    <h2>${STORE[questionNum].question}</h2>
+    <form>
+      <fieldset>
+        <label>
+          <input name="choice" type="radio" required value="${STORE[questionNum].choices[0]}">
+
+          <span>${STORE[questionNum].choices[0]}</span>
+        </label>
+
+        <label>
+          <input name="choice" type="radio" required value="${STORE[questionNum].choices[1]}">
+
+          <span>${STORE[questionNum].choices[1]}</span>
+        </label>
+
+        <label>
+          <input name="choice" type="radio" required value="${STORE[questionNum].choices[2]}">
+
+          <span>${STORE[questionNum].choices[2]}</span>
+        </label>
+
+        <label>
+          <input name="choice" type="radio" required value="${STORE[questionNum].choices[3]}">
+
+          <span>${STORE[questionNum].choices[3]}</span>
+        </label>
+
+        <button type="submit" class="enter-button"><span class="button-text">ENTER!</span></button>
+
+
+      </fieldset>
+    </form>
+    </div>`);
+
+  } else {
+      showResults()
+      console.log(`questionNum is ${questionNum}`)
+      runTimes++
+      console.log(`else is running ${runTimes}`)
+  }
+
+}
+
+let runTimes = 0;
+
+function showResults() {
+  runTimes++
+  console.log(`show results has run ${runTimes}`)
+  if (currentScore >= 5 && currentScore < 8) {
+    console.log("inserting results")
+    $('.film-image').attr("src", "Assets/kevinscream.jpeg")
+    $('.content').html(`<div class="result-message"><span>You got ${currentScore} right answer(s)</span><span>You still have some work to do if you want be a real DIT</span></div><button><span>RESTART!</span></button>`)
+    $('.next').css('display', 'none')
+    $('.correct-ans').css('display', 'none')
+  } else if (currentScore < 5) {
+    console.log("inserting results")
+    $('.film-image').attr("src", "Assets/mistake.jpeg")
+    $('.content').append(`<div class="result-message"><span class="result-text">You got ${currentScore} right answer(s)</span><span class="result-text">Are you sure you want to be DIT?</span><span class="result-text">Perhaps you've made a huge mistake</span></div><button class="quiz-button restart"><span class="button-text">RESTART!</span></button>`)
+    $('.next').css('display', 'none')
+    $('.correct-ans').css('display', 'none')
+  } else if (currentScore >=8 ) {
+    console.log("inserting results")
+    $('.film-image').attr("src", "Assets/troy.jpeg")
+    $('.next').css('display', 'none')
+    $('.correct-ans').css('display', 'none')
+    $('.content').append(`<div class="result-message"><span class="result-text">You got ${currentScore} right answer(s)</span><span class="result-text">You are ready to offload with the best of them!</span></div><button class="quiz-button restart"><span class="button-text">RESTART!</span></button>`)
+
+  }
+  $('.film-image').removeClass('hide')
+  handleRestart()
+}
+
+function handleRestart() {
+  $('.content').on('click', '.restart', function(){
+      location.reload()
+  })
+}
+
+function nextQuestion() {
+  runTimes++
+  console.log(`nextQuestion is running ${runTimes}`)
+  questionNum++
+  $('.question-num').text(questionNum)
+  $('.content').unbind().on('click', '.next', function(event){
+    console.log('next question button pressed')
+    removeAnswerImg()
+    showQuiz()
+  })
+}
+
+function updateScore() {
+  currentScore++
+  $('.user-score').text(currentScore)
+}
+
+function removeAnswerImg() {
+  $('.film-image').attr('src', 'Assets/film-strip2.png')
+}
+
+function makeNextButton() {
+  $('.content').append('<button class="quiz-button next"><span class="button-text">NEXT!</span>')
+}
+
+function showCorrectAnsImg() {
+  if ( $('.film-image').attr('src') === "Assets/film-strip2.png" ) {
+    $('.film-image').attr('src', 'Assets/Oscars-icon.jpg')
+  }
+  $('.quiz').addClass('hide')
+  makeNextButton()
+  nextQuestion()
+
+}
+
+function showWrongAnsImg() {
+  if ( $('.film-image').attr('src') === "Assets/film-strip2.png" ) {
+    $('.film-image').attr('src', 'Assets/Razzie-icon.jpg')
+    $('.content').prepend(`<div class="correct-ans"><span class="correct-text">CORRECT ANSWER: ${STORE[questionNum].correctAnswer}</span></div>`)
+  }
+  $('.quiz').addClass('hide')
+  makeNextButton()
+  nextQuestion()
+}
+
+function ansCorrect() {
+  console.log("correct answer!")
+  updateScore()
+  showCorrectAnsImg()
+}
+
+function wrongAnswer() {
+  showWrongAnsImg()
+}
+
+function handleAnswerSubmit() {
+  $('form').on('submit', function(event){
+    event.preventDefault()
+    console.log('enter pressed')
+    let userAnswer = $('input:checked').val()
+    console.log(`user anseer is ${userAnswer}`)
+    let correctAnswer = `${STORE[questionNum].correctAnswer}`
+    console.log(`correct answer is ${correctAnswer}`)
+
+    if (userAnswer === correctAnswer) {
+      ansCorrect()
+    } else {
+      wrongAnswer()
+    }
+
+  })
+}
+
+
+
+
+function showQuiz() {
+  console.log('showquiz running')
+  $('.form').attr('style', 'display: block');
+
+  buildQuestion();
+  handleAnswerSubmit();
+}
+
+
+
+
+function startQuiz() {
+  $('.content').html(`<div class="content begin-quiz">
+  <h1 class="title">DIGITAL IMAGE TECHNICIAN QUIZ</h1>
+  <button class="quiz-button begin">
+    <span class="button-text">START QUIZ!</span>
+  </button>
+  </div>`)
+
+  hideBeginQuiz();
+
+}
+
+
+$(startQuiz);
---
+++ store.js
@@ -0,0 +1,125 @@
+const STORE = [
+
+  {
+    question: 'What is Macbeth Chart used for?',
+
+    choices: [ 'Training your eyes to recognize colors',
+               'Calibrating a monitor to display the correct colors',
+               'Memorizing the color grid',
+               'Refreshing your eyes to accurate color when they get tired of looking at a monitor'
+
+    ],
+
+    correctAnswer: 'Calibrating a monitor to display the correct colors',
+
+  },
+  {
+
+    question: 'Dailies refer to?',
+
+    choices: ['Cast and crew who only work for one day',
+              'The tasks that are done everyday on a film set',
+              'The unedited footage shot each day',
+              'The food that is thrown out at the end of each shoot day'],
+
+    correctAnswer: 'The unedited footage shot each day',
+
+  },
+  {
+    question: 'What is the proper way to offload media files?',
+
+    choices: ['Drag and drop via Finder or File Explorer',
+              'Use dedicated offloading software that offers checksum verification',
+              'Move files one by one and play each file to verify the entire files has transferred',
+              'Clone each media card to a seperate hard drive via Carbon Copy Cloner'
+],
+
+    correctAnswer: 'Use dedicated offloading software that offers checksum verification',
+
+  },
+  {
+    question: 'What is a onelight color correction?',
+
+    choices: ['A color correction applied to a scene with only one light source',
+              'A color correction applied by shining a single light source through a color filter',
+              'A color correction used to assess the quality of the days shooting schedule',
+              'A color correction on only one shot in a scene'
+    ],
+
+    correctAnswer: 'A color correction used to assess the quality of the days shooting schedule',
+
+  },
+  {
+    question: 'What does a Histogram tell you about an image?',
+
+    choices: ['The amount of pixels in an image at each brightness level between dark and light',
+              'The resolution of the image',
+              'The correct ISO of the image',
+              'A range of acceptable exposure values'
+    ],
+
+    correctAnswer: 'The amount of pixels in an image at each brightness level between dark and light',
+
+  },
+  {
+    question: 'What is a primary color correction?',
+
+    choices: ['The first color correction done in a session',
+              'A general correction made to the overall look of the image',
+              'The most important color correction made',
+              'The most time consuming color correction to make'
+    ],
+
+    correctAnswer: 'A general correction made to the overall look of the image',
+
+  },
+  {
+    question: 'What is a secondary color correction?',
+
+    choices: ['The second color correction made in a session',
+              'An optional color correction',
+              'The easiest color correction to make',
+              'A color correction made to only one color in the scene'
+],
+
+    correctAnswer: 'A color correction made to only one color in the scene',
+
+  },
+  {
+    question: 'Adjusting the Lift of an image has what effect?',
+
+    choices: ['Adjusting the images black level',
+              'Makes the image brighter',
+              'Makes the image darker',
+              'Makes the colors more saturated'
+    ],
+
+    correctAnswer: 'Adjusting the images black level',
+
+  },
+  {
+    question: 'Adjusting the Gamma of an image affects what part of the image?',
+
+    choices: ['Shadows',
+              'Highlights',
+              'Midtones',
+              'Color'
+    ],
+
+    correctAnswer: 'Midtones',
+
+  },
+  {
+    question: 'Adjusting the gain affects what part of the image?',
+
+    choices: ['Highlights',
+              'Midtones',
+              'Color',
+              'Shadows'
+    ],
+
+    correctAnswer:'Highlights',
+
+  }
+
+]
